beef72c2bca908b24983bd730cc511dbde028cef
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-11-12 20:06:49 -0300

    MVC

--- mvc/App/Controllers/Page/Page.php
+++ mvc/App/Controllers/Page/Page.php
@@ -4,6 +4,26 @@ namespace App\Controllers\Page;
 
 class Page {
     /**
-     * Método Responsável por renderizar 
+     * Método Responsável por verificar a view
+     * @param string $view
+     * @return string
      */
+    public static function getContentView($view) {
+        $content = file_get_contents("../../resouces/Viw". $view .".html");
+
+        return $content;
+    }
+
+    /**
+     * Método Responsável por renderizar a view
+     * @param string $view
+     * @param array $data
+     * @return string
+     */
+    public static function render($view, $data = []) {
+        $content = self::getContentView($view);
+
+        return $content;
+    }
+
 }
